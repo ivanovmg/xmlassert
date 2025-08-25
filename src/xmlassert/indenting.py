3fb03315f2bd2ff453e14ef5c9d1660221a26010
Maxim Ivanov <ivanovmg@gmail.com>
2025-08-25 14:29:01 +0700

    REF: simplify indent

--- src/xmlassert/indenting.py
+++ src/xmlassert/indenting.py
@@ -12,63 +12,29 @@ __all__ = [
 
 
 def indent(elem: Element, level: int = 0) -> None:
-    """Recursively indent XML elements using visitor factory"""
-    factory = IndentVisitorFactory()
-    _traverse_with_factory(elem, factory, level)
+    """
+    Recursively indent XML elements with consistent spacing.
+    Based on the standard ElementTree indentation approach.
+    """
+    # Set indentation for current element
+    indent_str = '\n' + '  ' * level
 
-
-class ElementVisitor(Protocol):
-    def visit(self, elem: Element, level: int) -> None: ...
-
-
-class IndentVisitorFactory:
-    """Factory that provides appropriate visitors based on element type"""
-
-    @staticmethod
-    def get_pre_visitor(elem: Element) -> ElementVisitor:
-        if len(elem):
-            return ParentElementVisitor()
-        return LeafElementVisitor()
-
-    @staticmethod
-    def get_post_visitor(elem: Element) -> ElementVisitor:
-        return PostProcessingVisitor()
-
-
-class ParentElementVisitor:
-    def visit(self, elem: Element, level: int) -> None:
-        indent_str = '  ' * level
+    if len(elem):
+        # If element has children
         if not elem.text or not elem.text.strip():
-            elem.text = '\n' + indent_str + '  '
+            elem.text = indent_str + '  '
         if not elem.tail or not elem.tail.strip():
-            elem.tail = '\n' + indent_str
+            elem.tail = indent_str
 
+        # Process children
+        for child in elem:
+            indent(child, level + 1)
 
-class LeafElementVisitor:
-    def visit(self, elem: Element, level: int) -> None:
+        # Set tail for the last child
+        if not elem[-1].tail or not elem[-1].tail.strip():
+            elem[-1].tail = indent_str
+
+    else:
+        # Leaf element
         if level and (not elem.tail or not elem.tail.strip()):
-            elem.tail = '\n' + '  ' * level
-
-
-class PostProcessingVisitor:
-    def visit(self, elem: Element, level: int) -> None:
-        if len(elem) and (not elem.tail or not elem.tail.strip()):
-            elem.tail = '\n' + '  ' * level
-
-
-def _traverse_with_factory(
-    elem: Element,
-    factory: IndentVisitorFactory,
-    level: int = 0,
-) -> None:
-    # Pre-visit with appropriate visitor
-    pre_visitor = factory.get_pre_visitor(elem)
-    pre_visitor.visit(elem, level)
-
-    # Process children
-    for child in elem:
-        _traverse_with_factory(child, factory, level + 1)
-
-    # Post-visit
-    post_visitor = factory.get_post_visitor(elem)
-    post_visitor.visit(elem, level)
+            elem.tail = indent_str
